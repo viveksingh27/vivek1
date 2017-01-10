29beac63d3f1bae9bdfa1d399ce48cfa1069abc7
viveksingh27 <vsingh27@nait.ca>
2017-01-09 18:57:15 -0700

    setup entities,DAL, Connectionstrings

---
+++ viveksolution/ChinookSystem/DAL/ChinookContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+using Chinook.data.Entities;
+#endregion
+namespace ChinookSystem.DAL
+{
+    internal class ChinookContext:DbContext
+    {
+        public ChinookContext():base("ChinookDB")
+        {
+
+        }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums 
+    } 
+}
